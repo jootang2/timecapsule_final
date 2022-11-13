738f5ad644cf7c56f15bbdbaec18cbab4c3f4bb0
jootang2 <joohwan.song15b@gmail.com>
2022-11-13 21:28:46 +0900

    :sparkles: feat : capsule 내부 글쓰기 기능

--- src/main/java/com/jootang2/timecapsule/controller/BoardController.java
+++ src/main/java/com/jootang2/timecapsule/controller/BoardController.java
@@ -2,13 +2,19 @@ package com.jootang2.timecapsule.controller;
 
 import com.jootang2.timecapsule.domain.Board;
 import com.jootang2.timecapsule.domain.Capsule;
+import com.jootang2.timecapsule.domain.SiteUser;
+import com.jootang2.timecapsule.dto.BoardDto;
 import com.jootang2.timecapsule.service.BoardService;
 import com.jootang2.timecapsule.service.CapsuleService;
+import com.jootang2.timecapsule.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -20,7 +26,7 @@ public class BoardController {
 
     private final CapsuleService capsuleService;
     private final BoardService boardService;
-
+    private final UserService userService;
     @GetMapping("/list")
     public String boardMain(@PathVariable Long capsuleId, Model model) {
         Capsule capsule = capsuleService.findById(capsuleId);
@@ -28,6 +34,25 @@ public class BoardController {
         model.addAttribute("capsule", capsule);
         model.addAttribute("boardList", boardList);
         return "board/boardList";
+    }
+
+    @GetMapping("/create")
+    public String boardCreate(BoardDto boardDto, @PathVariable Long capsuleId, Model model) {
+        Capsule capsule = capsuleService.findById(capsuleId);
+        model.addAttribute("capsule", capsule);
+
+        return "board/boardWriteForm";
+
+    }
+
+    @PostMapping("/create")
+    public String boardCreate(BoardDto boardDto, Model model, @PathVariable Long capsuleId, Capsule capsule) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String userName = auth.getName();
+        SiteUser user = userService.findByName(userName);
+        capsule = capsuleService.findById(capsuleId);
+        boardService.create(boardDto, capsule, user);
+        return "redirect:/%d/board/list".formatted(capsuleId);
     }
 
 }
--- src/main/java/com/jootang2/timecapsule/service/BoardService.java
+++ src/main/java/com/jootang2/timecapsule/service/BoardService.java
@@ -1,6 +1,9 @@
 package com.jootang2.timecapsule.service;
 
 import com.jootang2.timecapsule.domain.Board;
+import com.jootang2.timecapsule.domain.Capsule;
+import com.jootang2.timecapsule.domain.SiteUser;
+import com.jootang2.timecapsule.dto.BoardDto;
 import com.jootang2.timecapsule.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,6 +18,18 @@ public class BoardService {
 
     public List<Board> findAll() {
         return boardRepository.findAll();
+    }
+
+    public void create(BoardDto boardDto , Capsule capsule, SiteUser user) {
+        Board board = new Board();
+        board.setCapsule(capsule);
+        board.setUser(user);
+        board.setBoardCategory(boardDto.getBoardCategory());
+        board.setBoardTitle(boardDto.getBoardTitle());
+        board.setBoardContent(boardDto.getBoardContent());
+        board.setPlaceX(boardDto.getPlaceX());
+        board.setPlaceY(boardDto.getPlaceY());
+        boardRepository.save(board);
     }
 
 }
